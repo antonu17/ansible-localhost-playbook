c52d23f32d84b5c1a517a4e214e74230285ddaf9
antonu17 <antonu17@gmail.com>
2016-11-14 12:35:12 +0600

    Add requred package installation

--- setup.sh
+++ setup.sh
@@ -10,12 +10,22 @@ update_role()
   test -d ${ROLES_DIR}/${1} && git pull || git clone ${GIT_PREFIX_URL}/${2}.git ${ROLES_DIR}/${1}
 }
 
+check_package() {
+    dpkg -l ${1} &>/dev/null && echo "${1} found" || ! echo -e "     ${1} not installed.\nInstalling ${1} requires root privileges" || sudo apt install -y ${1}
+}
+
+echo "=== Check required packages ==="
+check_package git
+check_package python-minimal
+echo
+
 echo "=== Update roles ==="
 update_role xenial-java ansible-java-role
 update_role xenial-openstack-clients ansible-openstack-clients
 update_role xenial-docker ansible-docker-role
 update_role xenial-virtualbox ansible-virtualbox-role
 update_role xenial-virtualization ansible-virtualization-role
+echo
 
 echo "=== Run playbook ==="
 ansible-playbook localhost.yml
